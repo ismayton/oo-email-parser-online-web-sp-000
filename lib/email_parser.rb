9ad83d7b9113b6773097d583be0bd33c928993d7
Ian Mayton <ian.mayton@gmail.com>
2020-07-24 22:43:31 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,7 +3,7 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 
-class EmailAddressParser
+class EmailAddressParser(string)
   
   attr_accessor :string, 
   
